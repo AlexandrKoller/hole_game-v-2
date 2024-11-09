b909d31275403d85dfd9d6301cb0b21c05bb091c
Alexandr Koller <wilhelmwigt@yandex.ru>
2024-11-09 13:57:17 +0500

    move index.js files

---
+++ index.js
@@ -0,0 +1,6 @@
+import './css/task.css';
+import './src/js/class.game'
+import './src/js/counter.game'
+import './src/js/app';
+
+// TODO: write your code in app.js
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import './css/task.css';
-import './js/class.game'
-import './js/counter.game'
-import './js/app';
-
-// TODO: write your code in app.js
